0a9fb57703180aabd20f50527168e72a7d37ff1d
Shubham Punekar <srp3003@gmail.com>
2018-06-07 11:35:06 +0530

    adds args

--- video_server.py
+++ video_server.py
@@ -8,6 +8,8 @@ import threading
 import cv2
 import numpy as np
 
+import argparse
+
 # from naoqi import ALProxy
 import random
 
@@ -58,7 +60,7 @@ class FrameRetriever(threading.Thread):
                 frame[:,:,0],frame[:,:,2] = temp[:,:,2],temp[:,:,0]
 
                 # write object to Queue
-                # self.frameQueue.put(frame)
+                self.frameQueue.put(frame)
                 
                 cv2.imshow('frame',frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -86,61 +88,84 @@ class FrameDispatcher(threading.Thread):
     def run(self):
         
         # testing
-        cap = cv2.VideoCapture(0)
+        # cap = cv2.VideoCapture(2)
               
         addr = (self.procIP, self.procPort)
+        skipframe = 1
 
         counter = 0
         while True:
-            # try:
-           
-            # test
-            ret, frame = cap.read()
-            
-            # frame = self.frameQueue.get()
-            
-            if(counter % 3 == 0):
-                # for testing, use webcam feed
-                # send every third frame
-                # ensures that prev frame is read by receiver
-                s = socket(AF_INET, SOCK_DGRAM)
-                cv2.imwrite("streamImg.jpg",frame)
-                f = open("streamImg.jpg", "rb")
-                data = f.read(1024)
-                while data:
-                    if(s.sendto(data, addr)):
-                        data = f.read(1024)
-                f.close()
-                s.close()
+            try:           
+                # test, remote the try/except block for Queue.Empty for testing
+                # ret, frame = cap.read()
                 
-            cv2.imshow('sender',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                frame = self.frameQueue.get()
+                
+                if(counter % skipframe == 0):
+                    # for testing, use webcam feed
+                    # send every third frame
+                    # ensures that prev frame is read by receiver
+                    s = socket(AF_INET, SOCK_DGRAM)
+                    cv2.imwrite("streamImg.jpg",frame)
+                    f = open("streamImg.jpg", "rb")
+                    data = f.read(1024)
+                    while data:
+                        if(s.sendto(data, addr)):
+                            data = f.read(1024)
+                    f.close()
+                    s.close()
+                    
+                cv2.imshow('sender',frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-            counter += 1
+                counter += 1
 
 
-            # except Queue.Empty:
-            #     pass     
+            except Queue.Empty:
+                pass     
        
 def main():
-    naoIP = '172.16.138.31'
-    naoPort = 9559
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nIP", help="nao IP")
+    parser.add_argument("--nP", help="nao Port")
+    parser.add_argument("--IP", help="proc IP")
+    parser.add_argument("--P", help="proc Port")
 
-    procIP = '127.0.0.1'
-    procPort = 4096
+    if(len(sys.argv)<2):
+        parser.print_usage()
+        sys.exit(1)
 
-    frameQueue = Queue.Queue()
+    args = parser.parse_args()
 
-    try:
-        # frameRetriever = FrameRetriever(naoIP, naoPort, frameQueue)
-        # frameDispatcher = FrameDispatcher(procIP, procPort, frameQueue)
-        frameDispatcher = FrameDispatcher(procIP,procPort)
+    if(args.nIP and
+        args.nP and
+        args.IP and
+        args.P):
+        
+        naoIP = args.nIP
+        naoPort = args.nP
+        # naoIP = '172.16.138.31'
+        # naoPort = 9559
 
-        # frameRetriever.start()
-        frameDispatcher.start()
-    except KeyboardInterrupt:
-        exit(1)
+        procIP = args.IP
+        procPort = args.P
+        # procIP = '127.0.0.1'
+        # procPort = 4096
+
+        frameQueue = Queue.Queue()
+
+        try:
+            frameRetriever = FrameRetriever(naoIP, naoPort, frameQueue)
+            frameDispatcher = FrameDispatcher(procIP, procPort, frameQueue)
+
+            frameRetriever.start()
+            frameDispatcher.start()
+        except KeyboardInterrupt:
+            exit(1)
+        
+
+    
 
 if __name__ == '__main__':
     main()
